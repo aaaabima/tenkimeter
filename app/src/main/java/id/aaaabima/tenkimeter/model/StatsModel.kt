fe09c8d9e6d2be0107a2f8cc6de960b5a702942b
aaaabima <aaaabim@gmail.com>
2024-07-18 11:52:22 +0700

    Add top stats data model

---
+++ app/src/main/java/id/aaaabima/tenkimeter/model/StatsModel.kt
@@ -0,0 +1,10 @@
+package id.aaaabima.tenkimeter.model
+
+/**
+ * @author Abim (aaaabim@gmail.com)
+ * @version StatsModel, v 0.1 7/17/2024 7:32 PM by Abim
+ */
+data class StatsModel(
+    val title: String,
+    val value: String
+)
